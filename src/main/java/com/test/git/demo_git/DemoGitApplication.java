65f43aa5f6984fbc8a322a97325a9a28def9b21f
Carlos Flores Martinez <cfloresm@anzen.com.mx>
2017-06-08 12:18:23 -0500

    Add init commit files.

---
+++ src/main/java/com/test/git/demo_git/DemoGitApplication.java
@@ -0,0 +1,12 @@
+package com.test.git.demo_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGitApplication.class, args);
+	}
+}
